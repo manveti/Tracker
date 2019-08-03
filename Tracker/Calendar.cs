f03e546f081aa6b52d730ca9eb9ab38a88e650b4
manveti <manveti@users.noreply.github.com>
2019-08-03 01:46:43 -0700

    Add a first few unit tests for time types

--- Tracker/Calendar.cs
+++ Tracker/Calendar.cs
@@ -12,6 +12,23 @@ namespace Tracker {
         public Interval(Decimal value) {
             this.value = value;
         }
+
+        public override bool Equals(object obj) {
+            if (obj is null) { return false; }
+            return this.value.Equals(((Interval)obj).value);
+        }
+
+        public static bool operator ==(Interval i1, Interval i2) {
+            if (ReferenceEquals(i1, i2)) { return true; }
+            if ((i1 is null) || (i2 is null)) { return false; }
+            return i1.value == i2.value;
+        }
+
+        public static bool operator !=(Interval i1, Interval i2) {
+            if (ReferenceEquals(i1, i2)) { return false; }
+            if ((i1 is null) || (i2 is null)) { return true; }
+            return i1.value != i2.value;
+        }
     }
 
 
@@ -27,12 +44,12 @@ namespace Tracker {
         }
 
         public virtual int CompareTo(Object obj) {
-            if (obj == null) { return 1; }
+            if (obj is null) { return 1; }
             return this.value.CompareTo(((Timestamp)obj).value);
         }
 
         public override bool Equals(object obj) {
-            if (obj == null) { return false; }
+            if (obj is null) { return false; }
             return this.value.Equals(((Timestamp)obj).value);
         }
 
@@ -41,12 +58,12 @@ namespace Tracker {
         }
 
         public static bool operator <(Timestamp t1, Timestamp t2) {
-            if ((t1 == null) || (t2 == null)) { return false; }
+            if ((t1 is null) || (t2 is null)) { return false; }
             return t1.CompareTo(t2) < 0;
         }
 
         public static bool operator <=(Timestamp t1, Timestamp t2) {
-            if ((t1 == null) || (t2 == null)) { return false; }
+            if ((t1 is null) || (t2 is null)) { return false; }
             return t1.CompareTo(t2) <= 0;
         }
 
@@ -56,32 +73,32 @@ namespace Tracker {
         }
 
         public static bool operator !=(Timestamp t1, Timestamp t2) {
-            if (ReferenceEquals(t1, t2)) { return true; }
+            if (ReferenceEquals(t1, t2)) { return false; }
             return t1.CompareTo(t2) != 0;
         }
 
         public static bool operator >=(Timestamp t1, Timestamp t2) {
-            if ((t1 == null) || (t2 == null)) { return false; }
+            if ((t1 is null) || (t2 is null)) { return false; }
             return t1.CompareTo(t2) >= 0;
         }
 
         public static bool operator >(Timestamp t1, Timestamp t2) {
-            if ((t1 == null) || (t2 == null)) { return false; }
+            if ((t1 is null) || (t2 is null)) { return false; }
             return t1.CompareTo(t2) > 0;
         }
 
         public static Timestamp operator +(Timestamp t, Interval amount) {
-            if ((t == null) || (amount == null)) { return null; }
+            if ((t is null) || (amount is null)) { return null; }
             return new Timestamp(t.value + amount.value);
         }
 
         public static Timestamp operator -(Timestamp t, Interval amount) {
-            if ((t == null) || (amount == null)) { return null; }
+            if ((t is null) || (amount is null)) { return null; }
             return new Timestamp(t.value - amount.value);
         }
 
         public static Interval operator -(Timestamp t1, Timestamp t2) {
-            if ((t1 == null) || (t2 == null)) { return null; }
+            if ((t1 is null) || (t2 is null)) { return null; }
             return new Interval(t1.value - t2.value);
         }
     }
---
+++ TrackerTests/CalendarTests.cs
@@ -0,0 +1,102 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Tracker;
+using System;
+
+
+namespace Tracker.Tests {
+    [TestClass]
+    public class IntervalTests {
+        [TestMethod]
+        public void test_compare() {
+            Decimal[] nums = { -1000, -5.823m, 0, 83.4m, 90210 };
+            Interval nada = null;
+
+            for (int i = 0; i < nums.Length; i++) {
+                Interval i1 = new Interval(nums[i]);
+
+                Assert.IsFalse(i1 == nada);
+                Assert.IsTrue(i1 != nada);
+
+                for (int j = 0; j < nums.Length; j++) {
+                    Interval i2 = new Interval(nums[j]);
+
+                    Assert.AreEqual(i1 == i2, nums[i] == nums[j]);
+                    Assert.AreEqual(i1 != i2, nums[i] != nums[j]);
+                }
+            }
+        }
+    }
+
+
+    [TestClass]
+    public class TimestampTests {
+        [TestMethod]
+        public void test_compare() {
+            Decimal[] nums = { 42, 0, -100, 10.7m, -1.23m };
+            Timestamp nada = null;
+
+            for (int i = 0; i < nums.Length; i++) {
+                Timestamp t1 = new Timestamp(nums[i]);
+
+                Assert.IsFalse(t1 > nada);
+                Assert.IsFalse(t1 >= nada);
+                Assert.IsFalse(t1 == nada);
+                Assert.IsTrue(t1 != nada);
+                Assert.IsFalse(t1 <= nada);
+                Assert.IsFalse(t1 < nada);
+
+                Assert.IsFalse(t1 > t1);
+                Assert.IsTrue(t1 >= t1);
+                Assert.IsTrue(t1 == t1);
+                Assert.IsFalse(t1 != t1);
+                Assert.IsTrue(t1 <= t1);
+                Assert.IsFalse(t1 < t1);
+
+                for (int j = 0; j < nums.Length; j++) {
+                    Timestamp t2 = new Timestamp(nums[j]);
+
+                    Assert.AreEqual(t1 > t2, nums[i] > nums[j]);
+                    Assert.AreEqual(t1 >= t2, nums[i] >= nums[j]);
+                    Assert.AreEqual(t1 == t2, nums[i] == nums[j]);
+                    Assert.AreEqual(t1 != t2, nums[i] != nums[j]);
+                    Assert.AreEqual(t1 <= t2, nums[i] <= nums[j]);
+                    Assert.AreEqual(t1 < t2, nums[i] < nums[j]);
+                    Assert.AreEqual(t1.CompareTo(t2), nums[i].CompareTo(nums[j]));
+                }
+            }
+        }
+
+        [TestMethod]
+        public void test_arithmetic() {
+            Decimal[] nums = { 1.23m, -456, 7890, 0, -3.14m };
+            Timestamp nada = null;
+            Interval nullInterval = null;
+
+            for (int i = 0; i < nums.Length; i++) {
+                Timestamp t1 = new Timestamp(nums[i]);
+                Interval i1 = new Interval(nums[i]);
+
+                Assert.IsNull(t1 + nullInterval);
+                Assert.IsNull(t1 - nullInterval);
+                Assert.IsNull(nada + i1);
+                Assert.IsNull(nada - i1);
+                Assert.IsNull(t1 - nada);
+                Assert.IsNull(nada - t1);
+
+                for (int j = 0; j < nums.Length; j++) {
+                    Timestamp t2 = new Timestamp(nums[j]);
+                    Interval i2 = new Interval(nums[j]);
+
+                    Assert.AreEqual(t1 + i2, new Timestamp(nums[i] + nums[j]));
+                    Assert.AreEqual(t1 - i2, new Timestamp(nums[i] - nums[j]));
+                    Interval isub = t1 - t2, newi = new Interval(nums[i] - nums[j]);
+                    Assert.AreEqual(isub.value, newi.value);
+                    Assert.IsTrue(isub.value == newi.value);
+                    Assert.AreEqual(isub, newi);
+                    Assert.AreEqual(t1 - t2, new Interval(nums[i] - nums[j]));
+                    Assert.AreEqual(t2 - t1, new Interval(nums[j] - nums[i]));
+                }
+            }
+        }
+    }
+}
